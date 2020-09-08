b61ffe3f33ac4575b6efe965480c40f2ce6f6b91
Cleber Silva <cleber_pesilva@yahoo.com.br>
2020-09-08 00:50:28 -0300

    feat: add dotenv and example to get env data in app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-import sys
-sys.path.append('./domain/availability/')
+import os
+from domain.availability.list_all_destinations import ListAllDestinations
+from dotenv import load_dotenv
+from pathlib import Path
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
-from list_all_destinations import ListAllDestinations
-
+#NAME: user_name
+#PASSWORD: user_password
+#CODE:user_code
 if __name__ == '__main__':
+    print(os.getenv('NAME'))
     soapheaders = {}
     list_all_destinations = ListAllDestinations(soapheaders)
     list_all_destinations.execute()
